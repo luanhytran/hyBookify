213b1d426064125d68e49c10f7513dff84c61758
Hy Tran <hy.tranluan@sai-digital.com>
2025-08-18 23:24:04 +0700

    Add project files.

---
+++ hyBookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace hyBookify.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        protected Entity(Guid id) => Id = id;
+
+        public Guid Id { get; init; }
+    }
+}
---
+++ hyBookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,23 @@
+﻿using hyBookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hyBookify.Domain.Apartments
+{
+    public sealed class Apartment : Entity
+    {
+        public Apartment(Guid id) : base(id) { }
+
+        public Name Name { get; set; }
+
+        public Description Description { get; private set; }
+
+        public Address Address { get; private set; }
+        public Email Email { get; private set; }
+    }
+}
