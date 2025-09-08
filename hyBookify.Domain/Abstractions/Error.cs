3ce562f1a7494149efbcfedd522c08b85ad82517
Hy Tran <hy.tran@b4value.net>
2025-09-08 17:51:05 +0700

    Add Error and Result classes for error handling and operation results

---
+++ hyBookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace hyBookify.Domain.Abstractions
+{
+    public record Error(string Code, string Name)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+
+        public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+    }
+}
