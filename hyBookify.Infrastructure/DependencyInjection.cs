ac933c92638d42eaf61c786acd17b733c2f0991e
Hy Tran <hy.tranluan@sai-digital.com>
2025-09-14 14:46:07 +0700

    Add DbContext configuration for PostgreSQL with snake_case naming convention

--- hyBookify.Infrastructure/DependencyInjection.cs
+++ hyBookify.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@ using hyBookify.Application.Abstractions.Clock;
 using hyBookify.Application.Abstractions.Email;
 using hyBookify.Infrastructure.Clock;
 using hyBookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,6 +15,15 @@ public static class DependencyInjection
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         
         services.AddTransient<IEmailService, EmailService>();
+        
+        var connectionString =
+            configuration.GetConnectionString("Database") ?? 
+            throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
         
         return services;
     }
