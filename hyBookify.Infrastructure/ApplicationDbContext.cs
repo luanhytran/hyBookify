6280280c7704dbed4ba3972c0b401d99178f0f83
Hy Tran <hy.tranluan@sai-digital.com>
2025-09-30 22:53:39 +0700

    Automatically applying entity configurations

--- hyBookify.Infrastructure/ApplicationDbContext.cs
+++ hyBookify.Infrastructure/ApplicationDbContext.cs
@@ -8,6 +8,12 @@ public class ApplicationDbContext : DbContext, IUnitOfWork
     public ApplicationDbContext(DbContextOptions options)
         : base(options)
     {
-        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
     }
 }
