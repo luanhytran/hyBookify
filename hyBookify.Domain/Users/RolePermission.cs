85cdc52d0c731e2f1d2aa4b83b6e56d1cae86c15
Hy Tran <hy.tranluan@sai-digital.com>
2025-10-17 08:57:31 +0700

    Add Role and Permission entities

---
+++ hyBookify.Domain/Users/RolePermission.cs
@@ -0,0 +1,9 @@
+﻿namespace hyBookify.Domain.Users
+{
+    public sealed class RolePermission
+    {
+        public int RoleId { get; set; }
+
+        public int PermissionId { get; set; }
+    }
+}
