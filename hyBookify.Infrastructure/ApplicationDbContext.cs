2b79579164993ccf12feca97a2987bed257007b3
Hy Tran <hy.tranluan@sai-digital.com>
2025-10-19 00:31:40 +0700

    Publishing domain events inside the Unit of Work

--- hyBookify.Infrastructure/ApplicationDbContext.cs
+++ hyBookify.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
 using hyBookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace hyBookify.Infrastructure;
 
 public class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options)
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,5 +20,36 @@ public class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventAsync();
+
+        return result;
+
+    }
+
+    private async Task PublishDomainEventAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
