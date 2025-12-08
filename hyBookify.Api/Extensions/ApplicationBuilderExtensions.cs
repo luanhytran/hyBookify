872ac25bd384d96a661ee6d4dd7eeed3795431e3
Hy Tran <hy.tranluan@sai-digital.com>
2025-12-08 23:39:44 +0700

    Creating EF migrations and applying them

---
+++ hyBookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+using hyBookify.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace hyBookify.Api.Extensions;
+
+internal static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
--- hyBookify.Api/Program.cs
+++ hyBookify.Api/Program.cs
@@ -17,6 +17,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
