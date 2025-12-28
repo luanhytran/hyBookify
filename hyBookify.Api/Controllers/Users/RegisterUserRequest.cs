35cce57ece273710d82afb2abeb01ba96bce7df2
Hy Tran <hy.tranluan@sai-digital.com>
2025-12-28 12:30:57 +0700

    Adding an API endpoint for registering a user

---
+++ hyBookify.Api/Controllers/Users/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace hyBookify.Api.Controllers.Users
+{
+    public sealed record RegisterUserRequest(
+        string Email,
+        string FirstName,
+        string LastName,
+        string Password);
+}
---
+++ hyBookify.Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using hyBookify.Application.Users.RegisterUser;
+using hyBookify.Domain.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hyBookify.Api.Controllers.Users
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public UsersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(
+        RegisterUserRequest request,
+        CancellationToken cancellationToken)
+        {
+            var command = new RegisterUserCommand(
+                request.Email,
+                request.FirstName,
+                request.LastName,
+                request.Password);
+
+            Result<Guid> result = await _sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return Ok(result.Value);
+        }
+    }
+}
